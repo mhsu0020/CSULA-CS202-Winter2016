a739d7d7f5b9ad10eb8ad1ce1e2c1735f7893808
pig0826usanj <pig0826usanj@gmail.com>
2016-01-25 20:40:14 -0800

    added example on empty array of reference types and arraylist

---
+++ Week4/ArrayAndArrayListOfObjectsExample.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class ArrayAndArrayListOfObjectsExample {
+
+	/*
+	 * Example of an inner class
+	 */
+	public class GeometricObject {
+		int height;
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * arrays provide default values depending on the type.
+		 *
+		 * Primitives have default values such as zero or false.
+		 *
+		 * arrays of reference types (Strings, Student, etc) have a default
+		 * value of null. You need to initialize an object and put it in the
+		 * array.
+		 *
+		 *
+		 */
+
+		// Example of empty string array
+		String[] exampleStringArray = new String[5];
+		for (String s : exampleStringArray) {
+			// All elements by default are null, aka nothing
+			System.out.println(s);
+		}
+
+		// Example of empty array of a type that you made up
+		GeometricObject[] exampleGeometryArray = new GeometricObject[5];
+		for (GeometricObject g : exampleGeometryArray) {
+			// All elements by default are null, aka nothing
+			System.out.println(g);
+		}
+
+		// The following code will cause a NullPointerException
+		// String newString = exampleStringArray[1].toUpperCase();
+
+		/*
+		 * Unlike an an array, an ArrayList has a dynamic size. It starts out as
+		 * size 0. As you add elements to it, the size increases dynamically
+		 *
+		 *
+		 */
+
+		ArrayList<String> exampleStringArrayList = new ArrayList<>();
+		System.out.println("initial size: "+exampleStringArrayList.size());
+
+		//Adding an element
+		exampleStringArrayList.add("test string 1233");
+		System.out.println("new size: "+exampleStringArrayList.size());
+
+
+
+
+	}
+
+}
